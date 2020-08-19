d833ae8ad88e8d4af1197ea2f1b2a01efad2c596
rahultesla <32484942+rahultesla@users.noreply.github.com>
2020-08-19 08:29:21 +0530

    Add files via upload

---
+++ models/calculation engines/extension.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import os
+import pyodbc
+class inputprocess():
+    def __init__(self,ids,inputs,id_name):
+        self.ids = ids
+        self.inputs = inputs
+        self.id_db = []
+        self.id_name = id_name
+        self.input_db = []
+        server = 'magdb.database.windows.net'
+        database = 'input_db'
+        username = 'letmetry'
+        password = 'Ins201799'
+        driver= '{ODBC Driver 17 for SQL Server}'
+        conn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+        for i in ids:
+            self.id_db.append(pd.read_sql_query("SELECT * FROM [dbo].["+str(i)+"]", conn))
+            self.id_db.name = i
+        for i in inputs:
+            self.input_db.append(pd.read_sql_query("SELECT * FROM [dbo].["+str(i)+"]", conn))
+            self.input_db.name = i
+            try:
+                self.input_db[i].drop(columns =["creation_date","updation_date"], inplace = True)
+            except:
+                pass
+
+            
+    def mergeall(self):
+        for i in range(len(self.input_db)):
+            for j in self.input_db[i].columns:
+                if j in self.id_name:
+                    self.input_db[i] = pd.merge(self.id_db[self.id_name.index(j)],self.input_db[i],on=j)
+                    self.input_db[i] = self.input_db[i].drop([j], axis=1)
+                try:
+                    self.input_db[j] = self.input_db[j].astype(float)
+                except:
+                    pass
+    def export(self):
+        if not os.path.isdir('exports'):
+            os.makedirs('exports')
+        for i in range(len(self.inputs)):
+            self.input_db[i].to_csv('exports/'+self.inputs[i]+'.csv')
+    def data(self,inputname):
+        i = self.inputs.index(inputname)
+        return self.input_db[i]
+    def pivot(self,inputname,col,val,indx=[]):
+        i = self.inputs.index(inputname)
+        if indx = []:
+            tc = list(self.input_db[i].columns)
+            tc.remove(val)
+            tc.remove(col)
+            indx = tc.copy()
+
+        self.input_db[i] = pd.pivot_table(self.input_db[i],index=indx, values=val,columns=col,aggfunc='sum')
+        self.input_db[i].reset_index(inplace=True)
+        for j in self.input_db[i].columns:
+            self.input_db[i] = self.input_db[i].rename(columns={j:str(j)})
+    def rename(self,inputname,from_to):
+        i = self.inputs.index(inputname)
+        self.input_db[i] = self.input_db[i].rename(columns=from_to)
+    
+            
+        
+        
